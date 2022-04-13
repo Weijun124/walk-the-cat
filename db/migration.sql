cb06732f98b46363f89a9398841115d9b934e9a2
IMedellin <i.j.r.medellin@gmail.com>
2022-04-13 15:49:19 -0500

    server halfway

---
+++ db/migration.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS card_table;
+DROP TABLE IF EXISTS item_table;
+CREATE TABLE card_table (id SERIAL PRIMARY KEY, title TEXT);
+CREATE TABLE item_table (
+  id SERIAL PRIMARY KEY,
+  card_id INT REFERENCES card_table(id),
+  description TEXT,
+  status_label BOOLEAN
+);
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO card_table(title)
+VALUES ('Todo');
+INSERT INTO item_table(card_id, description, status_label)
+VALUES (1, 'Walk the cat', false);
+INSERT INTO item_table(card_id, description, status_label)
+VALUES (1, 'Wash car', false);
+INSERT INTO card_table(title)
+VALUES ('Task Completed');
+INSERT INTO item_table(card_id, description, status_label)
+VALUES (2, 'Sweep the floors', true);
