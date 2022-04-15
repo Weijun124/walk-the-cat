2f99ab1857f85e5d413aed0b376a371e19a9ffda
Colt Skeen <colton.skeen@gmail.com>
2022-04-15 16:32:34 -0400

    Added arrow icons, drop down menus, need to fix menu only displaying on nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import '../App.css';
 import axios from 'axios';
 
 
 const NavBar = (props) => {
     console.log(props);
+    const dropdownRef = useRef(null);
+    const dropdownRef1 = useRef(null);
+    const dropdownRef2 = useRef(null);
+    const [isActive, setIsActive] = useState(false);
+    const [isWsActive, setIsWsActive] = useState(false);
+    const [isRecActive, setIsRecActive] = useState(false);
+    const [isMorActive, setIsMorActive] = useState(false);
+
+    const onClick = (e) => {
+        // console.log(e.target.id === "rec");
+        if(e.target.id === "ws") setIsWsActive(!isWsActive);
+        if(e.target.id === "rec") setIsRecActive(!isRecActive);
+        if(e.target.id === "mor") setIsMorActive(!isMorActive);
+        
+    }
+    
+    // console.log(isActive);
     return (
         <header className="nav-bar">
             <div className="nav-left">
@@ -22,11 +39,32 @@ const NavBar = (props) => {
                 <div className="logo">
                     <img src="./pictures/cat-logo.png" />
                 </div>
-                <nav>
-                    <a className="nav-link">Workspaces</a>
-                    <a className="nav-link">Recent</a>
-                    <a className="nav-link">More</a>
-                </nav>
+                <div className="dropdown-container">
+                    <div className="dropdown">
+                        <button onClick={onClick} id="ws" className="nav-dropdown-btn">Workspaces <img className="arrow-icon" src="./pictures/down-arrow-icon.png"/></button>
+                        <nav ref={dropdownRef} className={`menu ${isWsActive ? 'active' : 'inactive'}`}>
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </nav>
+                    </div>
+                    <div className="dropdown">
+                        <button onClick={onClick} id="rec" className="nav-dropdown-btn">Recent <img className="arrow-icon" src="./pictures/down-arrow-icon.png"/></button>
+                        <nav ref={dropdownRef1} className={`menu ${isRecActive ? 'active' : 'inactive'}`}>
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </nav>
+                    </div>
+                    <div className="dropdown">
+                        <button onClick={onClick} id="mor" className="nav-dropdown-btn">More <img className="arrow-icon" src="./pictures/down-arrow-icon.png"/></button>
+                        <nav ref={dropdownRef2} className={`menu ${isMorActive ? 'active' : 'inactive'}`}>
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </nav>
+                    </div>
+                </div>
                 <button className="nav-create-button">Create</button>
             </div>
             <div className="nav-right">
