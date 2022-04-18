9224a0a0aa2aea75261767d313429a988bf4a777
IMedellin <i.j.r.medellin@gmail.com>
2022-04-18 17:06:10 -0500

    Double Tap feature Created

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Status from "./Status"
+import axios from "axios";
 
 const Card = props => {
+
+  const [toggle, setToggle] = useState(true);
+  const [description, setDescription] = useState(props.data);
+
+  const handleEdit = (update) => {
+    axios.patch("/item", update);
+  }
+
   return (
-    <div className={"item-container"}>
-      <Status status={props.status}/>
-      {props.data}
+    <div className={"item-container"} onDoubleClick={() => { setToggle(false) }}>
+      <Status status={props.status} />
+      {toggle ? (
+        description
+      ) : (
+        <input type='text' value={description} onChange={(event) => {
+          setDescription(event.target.value)
+        }} onKeyDown={(event) => {
+          if (event.keyCode === 13) {
+            const update = {
+              description: event.target.value,
+              item_id: props.id
+            }
+            handleEdit(update)
+            setToggle(true)
+          }
+        }} />
+      )
+
+      }
     </div>
-  )}
+  )
+}
 
 export default Card;
